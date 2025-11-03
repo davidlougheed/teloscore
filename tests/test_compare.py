8f1b161ea2fddcf1081423405f1aff347a8c8824
David Lougheed <david.lougheed@gmail.com>
2025-11-03 10:21:59 -0500

    test: add hg002-hg004 scoring 1 test

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -1,5 +1,4 @@
 import io
-import sys
 from pathlib import Path
 from teloscore import compare, scoring
 
@@ -9,10 +8,14 @@ HG002 = TEST_DATA_DIR / "tlens_by_allele_HG002.tsv"
 HG003 = TEST_DATA_DIR / "tlens_by_allele_HG003.tsv"
 HG004 = TEST_DATA_DIR / "tlens_by_allele_HG004.tsv"
 
-OUT_0_1_0 = TEST_DATA_DIR / "out_0_1_0.tsv"
+OUT_0_1_0_HG002_HG003 = TEST_DATA_DIR / "out_0_1_0_HG002_HG003.tsv"
+OUT_0_1_0_HG002_HG004 = TEST_DATA_DIR / "out_0_1_0_HG002_HG004.tsv"
 
-with open(OUT_0_1_0, "r") as ofh:
-    OUT_0_1_0_DATA = ofh.read()
+with open(OUT_0_1_0_HG002_HG003, "r") as ofh:
+    OUT_0_1_0_HG002_HG003_DATA = ofh.read()
+
+with open(OUT_0_1_0_HG002_HG004, "r") as ofh:
+    OUT_0_1_0_HG002_HG004_DATA = ofh.read()
 
 
 def test_loading():
@@ -22,15 +25,24 @@ def test_loading():
     assert len(hg003_alleles) == 43
 
 
-def test_compare_s1():
+def test_compare_s1_hg002_hg003():
     with io.StringIO() as fh:
         compare.compare_samples(scoring.Scoring1(), HG002, HG003, fh)
         fh.seek(0)
         matrix_out = fh.read()
-    assert matrix_out == OUT_0_1_0_DATA
+    assert matrix_out == OUT_0_1_0_HG002_HG003_DATA
+
+
+def test_compare_s1_hg002_hg004():
+    with io.StringIO() as fh:
+        compare.compare_samples(scoring.Scoring1(), HG002, HG004, fh)
+        fh.seek(0)
+        matrix_out = fh.read()
+    assert matrix_out == OUT_0_1_0_HG002_HG004_DATA
 
 
 def test_compare_s2():
     with io.StringIO() as fh:
         compare.compare_samples(scoring.Scoring2(), HG002, HG004, fh)
+        fh.seek(0)
         # TODO
